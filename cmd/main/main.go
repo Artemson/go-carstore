60892425424ae5770089dbf5840f92322b23178a
Artemson <artemvetlugaev@gmail.com>
2023-06-30 00:44:17 +0300

    Add code in all files

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/Artemson/go-carstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterCarStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
